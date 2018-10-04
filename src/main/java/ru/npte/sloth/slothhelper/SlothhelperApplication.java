95850e910817ee3b7b88fccbfc3ce707f2aed4e7
npte <kamazoh@inbox.ru>
2018-10-04 13:51:45 +0000

    test2

--- src/main/java/ru/npte/sloth/slothhelper/SlothhelperApplication.java
+++ src/main/java/ru/npte/sloth/slothhelper/SlothhelperApplication.java
@@ -2,8 +2,6 @@ package ru.npte.sloth.slothhelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class SlothhelperApplication {
@@ -11,10 +9,4 @@ public class SlothhelperApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SlothhelperApplication.class, args);
 	}
-
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String getHello() {
-        return "hello world";
-    }
 }
