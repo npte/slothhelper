75cf3fe73042bf32889367dbed36d8f77bbabea2
npte <kamazoh@inbox.ru>
2018-10-04 15:55:27 +0300

    Initial commit

---
+++ src/main/java/ru/npte/sloth/slothhelper/SlothhelperApplication.java
@@ -0,0 +1,12 @@
+package ru.npte.sloth.slothhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlothhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlothhelperApplication.class, args);
+	}
+}
